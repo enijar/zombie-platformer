ddd80122cd4dc7de6f74d4566e6e180288835c8e
Enijar <TheEnijar@gmail.com>
2022-05-14 16:09:08 +0100

    chore: add player component

--- src/game/game.tsx
+++ src/game/game.tsx
@@ -5,6 +5,7 @@ import Platform from "@/game/components/platform";
 import Camera from "@/game/components/camera";
 import Scene from "@/game/components/scene";
 import Zombie from "@/game/components/zombie";
+import Player from "@/game/components/player";
 
 export default function Game() {
   return (
@@ -14,9 +15,15 @@ export default function Game() {
           position={config.scene.position}
           rotation={config.scene.rotation}
         >
-          <Zombie id={1} position={[0, 0, 0]} />
+          <Zombie id={1} position={[0.75, 0, 0]} />
           <Platform size={config.platform.size} />
         </Scene>
+        <group
+          position={config.scene.position}
+          rotation={config.scene.rotation}
+        >
+          <Player position={[0, -1.5, 0.35]} />
+        </group>
         <Camera position={config.camera.position} />
         <ambientLight />
         <pointLight position={[0, 5, 5]} />
